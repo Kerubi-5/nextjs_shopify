99ea0d37ce759c0774ad878e19b28c8441c7eedd
John Kim A. Querobines <querobinesj@gmail.com>
2022-04-12 17:11:25 +0800

    fix: removes ts(2339) error

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 
-const Layout: FC = ({ children }) => {
+const Layout: FC<{ children: JSX.Element }> = ({ children }) => {
   return <div className="layout">{children}</div>;
 };
 

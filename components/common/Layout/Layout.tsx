7f537419cc3c3f82c8b50be2dd9d0386b288c9d1
John Kim Querobines <querobinesj@gmail.com>
2022-04-13 10:08:00 +0800

    style: adds layout styles

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,7 +1,12 @@
 import { FunctionComponent, ReactNode } from "react";
+import s from "./Layout.module.css";
 
 const Layout: FunctionComponent<{ children: ReactNode }> = ({ children }) => {
-  return <div className="layout">{children}</div>;
+  return (
+    <>
+      <main className={s.root}>{children}</main>
+    </>
+  );
 };
 
 export default Layout;

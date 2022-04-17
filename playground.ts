5fbbb6598891f5a8f18689f05a202ccef07c276a
John Kim A. Querobines <querobinesj@gmail.com>
2022-04-17 19:46:00 +0800

    refactor: for presentation

--- playground.ts
+++ playground.ts
@@ -1,83 +1,88 @@
 export default function play() {
   // * GOOD FOR DATA STRUCTURES
-  //   interface IPerson {
-  //     name: string;
-  //     age: number;
+  // interface Person {
+  //   name: string;
+  //   age: number;
+  // }
+  // type Human = {
+  //   name: string;
+  //   age: number;
+  // };
+  // class PersonClass {
+  //   name: string;
+  //   age: number;
+  //   constructor(name: string, age: number) {
+  //     this.name = name;
+  //     this.age = age;
   //   }
-  //   type Human = {
-  //     name: string;
-  //     age: number;
-  //   };
-  //   class PersonClass {
-  //     name: string;
-  //     age: number;
-  //     constructor(name: string, age: number) {
-  //       this.name = name;
-  //       this.age = age;
-  //     }
-  //   }
+  // }
   // * TYPE IS GOOD FOR FUNCTIONS AND ALIASES
-  //   type PersonLoggerFN = (person: Person) => string;
-  //   const personLogger: PersonLoggerFN = (person: Person) => {
-  //     return person.name;
-  //   };
+  // type PersonLoggerFN = (person: Person) => string;
+  // const personLogger: PersonLoggerFN = (person: Person) => {
+  //   return "Hello world";
+  // };
   // * OPTIONAL PARAMETERS
-  // * GENERIC FUNCTIONS
-  // function printInfo({[key string]: any}): void
-  //
-  //   function printInfo(someObject: { [key: string]: unknown }) {
-  //     if (typeof someObject.age === "string") {
-  //       someObject.age.toUpperCase();
-  //     }
+  // TODO: Change this GENERIC FUNCTIONS NOT WORKING
+  // function printInfo(someObject: { [key: string]: unknown }) {
+  //   if (typeof someObject.age === "string") {
+  //     someObject.age.toUpperCase();
   //   }
+  // }
   // * EXTENDS INTERFACE
-  //   interface IPerson {
-  //     kind: "business" | "academic";
-  //     name: string;
-  //     age: number;
+  // interface IPerson {
+  //   kind: "business" | "academic" | "developer"; // business or academic
+  //   name: string;
+  //   age: number;
+  // }
+  // interface BusinessPerson extends IPerson {
+  //   kind: "business";
+  //   salary: number;
+  // }
+  // interface AcademicPerson extends IPerson {
+  //   kind: "academic";
+  //   publications: string[];
+  // }
+  // * NEVER CASE
+  // interface DeveloperPerson extends IPerson {
+  //   kind: "developer";
+  //   skills: string[];
+  // }
+  // * EXTENDS TYPES
+  // type Car = {
+  //   name: string;
+  // };
+  // type RaceCar = {
+  //   speed: number;
+  // } & Car & { mileage: number };
+  // RaceCar = name, speed, mileage
+  // * JOIN OPERATIONS
+  // type Human = BusinessPerson | AcademicPerson;
+  // // ! ERROR SHOULD BE ABLE TO ACCESS ALL PROPERTIES
+  // const person: Human = {
+  //   // You can access all properties from Business and Academic
+  //   name: "John",
+  //   age: 30,
+  //   salary: 1000,
+  //   publications: ["book1", "book2"],
+  // };
+  // const logHuman = (h: Human) => {
+  //   // Only common values can be accessed
+  //   console.log(h.name);
+  //   // Narrow to specify what to see
+  //   if ("salary" in h) {
+  //     console.log(h.salary);
+  //   } else if ("publications" in h) {
+  //     console.log(h.publications);
   //   }
-  //   interface BusinessPerson extends Person {
-  //     kind: "business";
-  //     salary: number;
+  //   if (h.kind === "academic") {
+  //     console.log(h.publications);
+  //   } else if (h.kind === "business") {
+  //     console.log(h.salary);
+  //   } else {
+  //     const _never: never = h;
+  //     return _never;
   //   }
-  //   interface AcademicPerson extends Person {
-  //     kind: "academic";
-  //     publications: string[];
-  //   }
-  //   // * EXTENDS TYPES
-  //   //   type Car = {
-  //   //     name: string;
-  //   //   };
-  //   //   type RaceCar = {
-  //   //     speed: number;
-  //   //   } & Car & { mileage: number };
-  //   // * JOIN OPERATIONS
-  //   type Human = BusinessPerson | AcademicPerson;
-  //   const person: Human = {
-  //     // You can access all properties from Business and Academic
-  //     name: "John",
-  //     age: 30,
-  //     salary: 1000,
-  //     publications: ["book1", "book2"],
-  //   };
-  //   const logHuman = (h: Human) => {
-  //     // Only common values can be accessed
-  //     console.log(h.name);
-  //     // Narrow to specify what to see
-  //     // if ("salary" in h) {
-  //     //   console.log(h.salary);
-  //     // } else if ("publications" in h) {
-  //     //   console.log(h.publications);
-  //     // }
-  //     // if (h.kind === "academic") {
-  //     //   console.log(h.publications);
-  //     // } else if (h.kind === "business") {
-  //     //   console.log(h.salary);
-  //     // } else {
-  //     //   const _never: never = h;
-  //     //   return _never;
-  //     // }
-  //   };
+  // };
   // * TYPECASTING
   // interface ISample {
   //   name: string;
@@ -88,44 +93,61 @@ export default function play() {
   // // let a = b as ISample;
   // // let a = <ISample>b;
   // * GENERIC ARRAY
-  //   function iterate(items: Array<string>) {
-  //     for (const item of items) {
-  //       console.log(item.toUpperCase());
-  //     }
+  // function iterate(items: Array<string>) {
+  //   for (const item of items) {
+  //     console.log(item.toUpperCase());
   //   }
-  //   iterate(["a", "b", "c"]);
+  // }
+  // iterate(["a", "b", "c"]);
   // * GENERIC EXTENDS
-  // interface IPerson {
+  // interface Person {
   //   name: string;
   //   age: number;
   //   city: string;
   // }
-  // interface IStudent extends IPerson {
+  // interface Student extends Person {
   //   grade: number;
+  // }
+  // interface BusinessPerson extends Person {
+  //   salary: number;
   // }
   // interface ILogger<T extends IPerson> {
   //   data: T;
   //   log(): void;
   // }
-  // const logger: ILogger<IStudent> = {
+  // const logger: ILogger<IBusinessPerson> = {
   //   data: {
   //     name: "John",
   //     age: 30,
   //     city: "New York",
-  //     grade: 10,
+  //     salary: 1000,
   //   },
   //   log() {
   //     console.log(this.data.name);
   //   },
   // };
   // * TERNARY EXTENDS
-  //   interface StudentInfo<T extends Student = Student> {
-  // type StudentInfo<T extends any = Student> = T extends Student ? {
-  //   data: T
-  //   grades: number[]
+  // interface Person {
+  //   name: string;
   // }
-  // } : string
-  // type Car = {engine: string}
+  // interface Student extends Person {
+  //   age: number;
+  // }
+  // interface PostGraduadeStudent extends Person {
+  //   age: number;
+  //   projects: string[];
+  // }
+  // type StudentInfo<T extends any = Student> = T extends Student
+  //   ? {
+  //       data: T;
+  //       grades: number[];
+  //     }
+  //   : string;
+  // type Car = { engine: string };
+  // function logStudentInfo(info: StudentInfo<Student>) {
+  //   console.log(info);
+  //   console.log(info);
+  // }
   // * SINGLE TYPES
   // type SingleType<T> = T extends any[] ? T[number] : unknown;
   // type Type1 = SingleType<string[]>;

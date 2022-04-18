0dbf42c98d703579bd02f776ebf7aa44ad74c40c
John Kim Querobines <querobinesj@gmail.com>
2022-04-18 10:46:15 +0800

    refactor: pre-presentation

--- playground.ts
+++ playground.ts
@@ -1,83 +1,85 @@
 export default function play() {
+  // ? WHY USE TYPESCRIPT
+  // refactor;
+  // documentation;
+  // debugging;
   // * GOOD FOR DATA STRUCTURES
-  //   interface IPerson {
-  //     name: string;
-  //     age: number;
+  // interface IPerson {
+  //   name: string;
+  //   age: number;
+  // }
+  // type Human = {
+  //   name: string;
+  //   age: number;
+  // };
+  // class PersonClass {
+  //   name: string;
+  //   age: number;
+  //   constructor(name: string, age: number) {
+  //     this.name = name;
+  //     this.age = age;
   //   }
-  //   type Human = {
-  //     name: string;
-  //     age: number;
-  //   };
-  //   class PersonClass {
-  //     name: string;
-  //     age: number;
-  //     constructor(name: string, age: number) {
-  //       this.name = name;
-  //       this.age = age;
-  //     }
-  //   }
+  // }
   // * TYPE IS GOOD FOR FUNCTIONS AND ALIASES
   //   type PersonLoggerFN = (person: Person) => string;
   //   const personLogger: PersonLoggerFN = (person: Person) => {
   //     return person.name;
   //   };
   // * OPTIONAL PARAMETERS
-  // * GENERIC FUNCTIONS
-  // function printInfo({[key string]: any}): void
-  //
-  //   function printInfo(someObject: { [key: string]: unknown }) {
-  //     if (typeof someObject.age === "string") {
-  //       someObject.age.toUpperCase();
-  //     }
-  //   }
+  // const person: IPerson = {
+  //   name: "John",
+  //   age: 30,
+  //   job: true,
+  // };
   // * EXTENDS INTERFACE
-  //   interface IPerson {
-  //     kind: "business" | "academic";
-  //     name: string;
-  //     age: number;
+  // interface Person {
+  //   kind: "business" | "academic";
+  //   name: string;
+  //   age: number;
+  // }
+  // interface BusinessPerson extends Person {
+  //   kind: "business";
+  //   salary: number;
+  // }
+  // interface AcademicPerson extends Person {
+  //   kind: "academic";
+  //   publications: string[];
+  // }
+  // * EXTENDS TYPES
+  // type Car = {
+  //   name: string;
+  // };
+  // type RaceCar = {
+  //   speed: number;
+  // } & Car & { mileage: number };
+  // * JOIN OPERATIONS
+  // type Human = BusinessPerson | AcademicPerson;
+  // const person: Human = {
+  //   // You can access all properties from Business and Academic
+  //   // kind: "academic",
+  //   name: "John",
+  //   age: 30,
+  //   // publications: ["book1", "book2"],
+  //   // salary: 1000,
+  // };
+  // const logHuman = (h: Human) => {
+  // Only common values can be accessed
+  // console.log(h.name);
+  // Narrow to specify what to see
+  //   if ("salary" in h) {
+  //     console.log(h.salary);
+  //   } else if ("publications" in h) {
+  //     console.log(h.publications);
   //   }
-  //   interface BusinessPerson extends Person {
-  //     kind: "business";
-  //     salary: number;
+  //   if (h.kind === "academic") {
+  //     console.log(h.publications);
+  //   } else if (h.kind === "business") {
+  //     console.log(h.salary);
+  //   } else {
+  //     const _never: never = h;
+  //     return _never;
   //   }
-  //   interface AcademicPerson extends Person {
-  //     kind: "academic";
-  //     publications: string[];
-  //   }
-  //   // * EXTENDS TYPES
-  //   //   type Car = {
-  //   //     name: string;
-  //   //   };
-  //   //   type RaceCar = {
-  //   //     speed: number;
-  //   //   } & Car & { mileage: number };
-  //   // * JOIN OPERATIONS
-  //   type Human = BusinessPerson | AcademicPerson;
-  //   const person: Human = {
-  //     // You can access all properties from Business and Academic
-  //     name: "John",
-  //     age: 30,
-  //     salary: 1000,
-  //     publications: ["book1", "book2"],
-  //   };
-  //   const logHuman = (h: Human) => {
-  //     // Only common values can be accessed
-  //     console.log(h.name);
-  //     // Narrow to specify what to see
-  //     // if ("salary" in h) {
-  //     //   console.log(h.salary);
-  //     // } else if ("publications" in h) {
-  //     //   console.log(h.publications);
-  //     // }
-  //     // if (h.kind === "academic") {
-  //     //   console.log(h.publications);
-  //     // } else if (h.kind === "business") {
-  //     //   console.log(h.salary);
-  //     // } else {
-  //     //   const _never: never = h;
-  //     //   return _never;
-  //     // }
-  //   };
+  // };
   // * TYPECASTING
   // interface ISample {
   //   name: string;

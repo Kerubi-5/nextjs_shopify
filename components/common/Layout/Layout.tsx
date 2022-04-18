05614fc2454ed8daebb5d47bb6c15f9536b5f600
John Kim Querobines <querobinesj@gmail.com>
2022-04-18 11:18:39 +0800

    feat: Cart and Wishlist

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,6 +1,8 @@
 import { FC } from "react";
 import s from "./Layout.module.css";
 import { Footer, Navbar } from "@components/common";
+import { Sidebar } from "@components/ui";
+import { CartSidebar } from "@components/cart";
 
 type Props = {
   children: React.ReactNode;
@@ -10,6 +12,9 @@ const Layout: FC<Props> = ({ children }) => {
   return (
     <div className={s.root}>
       <Navbar />
+      <Sidebar>
+        <CartSidebar />
+      </Sidebar>
       <main className="fit">{children}</main>
       <Footer />
     </div>

c0b0bb1f5bd550fed01e63f2ca27bb4f0d8614f9
John Kim Querobines <querobinesj@gmail.com>
2022-04-18 14:27:16 +0800

    feat: context and usereducer

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -3,16 +3,19 @@ import s from "./Layout.module.css";
 import { Footer, Navbar } from "@components/common";
 import { Sidebar } from "@components/ui";
 import { CartSidebar } from "@components/cart";
+import { useUI } from "@components/ui/context";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Layout: FC<Props> = ({ children }) => {
+  const { isSidebarOpen, closeSidebar } = useUI();
+
   return (
     <div className={s.root}>
       <Navbar />
-      <Sidebar>
+      <Sidebar onClose={closeSidebar} isOpen={isSidebarOpen}>
         <CartSidebar />
       </Sidebar>
       <main className="fit">{children}</main>

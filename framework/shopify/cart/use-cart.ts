29297b25d481091fbcac70cfb8336d64f887a60a
John Kim A. Querobines <querobinesj@gmail.com>
2022-04-21 09:13:03 +0800

    feat: checkout query

--- framework/shopify/cart/use-cart.ts
+++ framework/shopify/cart/use-cart.ts
@@ -1,12 +1,12 @@
 import useCart from "@common/cart/use-cart";
-import { createCheckout } from "@framework/utils";
+import { createCheckout, getCheckoutQuery } from "@framework/utils";
 
 export default useCart;
 
 export const handler = {
   fetchOptions: {
     // get checkout query
-    query: "query { hello }",
+    query: getCheckoutQuery,
   },
   async fetcher({ fetch, options, input: { checkoutId } }: any) {
     let checkout;
@@ -15,6 +15,9 @@ export const handler = {
     if (checkoutId) {
       const { data } = await fetch({
         ...options,
+        variables: {
+          checkoutId,
+        },
       });
 
       checkout = data.node;
